2d84974b4065b21c1187fe3930e657a9fd7be2af
Rhythmboy <173650385+LemonboyC3@users.noreply.github.com>
2026-06-11 18:19:18 +0800

    Started building part of table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from wsgiref import headers
 import pandas as pd 
 import csv
 
@@ -6,7 +7,7 @@ import csv
 # Begin declarations
 timetabledata = pd.read_csv('timetabledata.csv')
 
-#print(timetabledata.head())
+#print(timetabledata.to_string(index=False))
 
 
 # Begin declaring functions and stuff
@@ -15,13 +16,13 @@ def clearterm():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def handle_input():
+    print_timetable()
     print("In viewing mode. Input q to quit, e to edit")
     valid_command = False
     while not valid_command:
         userinput = str(input(">"))
         match userinput:
             case "q":
-                valid_command = True
                 pass
             case "e":
                 edit_mode_UI()
@@ -29,10 +30,34 @@ def handle_input():
                 print("Invalid command. Input q to quit, e to edit")
             
     
+def print_timetable():
+    # Yes I'm aware of libraries that can work, but I don't want to wrestle with pip
+    cellwidth = 11
+    tablewidth = cellwidth * 7
+    headers = timetabledata.columns.tolist()
+
+    # Print pretty little top part
+    for header in headers:
+        print("+",end="")
+        for i in range(cellwidth+1):
+            print("─", end="")
+    print("+")
+
+    # Print header row
+    for header in headers:
+        print("| " + header, end="")
+        if len(header) < cellwidth:
+            for i in range(cellwidth - len(header)):
+                print(" ", end="")
+    print("|")
+
+    
+    print()
+    print()
 
 
 def edit_mode_UI():
-    pass
+    clearterm()
 
 def edit_data():
     pass
