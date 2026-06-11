f75084f54e9173f7f0432e72c7a84fe7ae15a3bf
Rhythmboy <173650385+LemonboyC3@users.noreply.github.com>
2026-06-11 18:40:40 +0800

    Table works

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import csv
 # Begin declarations
 timetabledata = pd.read_csv('timetabledata.csv')
 
+cellwidth = 11
+tablewidth = cellwidth * 7
 #print(timetabledata.to_string(index=False))
 
 
 # Begin declaring functions and stuff
-def clearterm():
+def clear_terminal():
     # 'cls' is for Windows, 'clear' is for Mac and Linux
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -29,35 +31,59 @@ def handle_input():
             case _:
                 print("Invalid command. Input q to quit, e to edit")
             
+# All functions below are used to print the timetable.
+# (print_timetable calls other functions for conciseness and mantainablilty)
     
 def print_timetable():
     # Yes I'm aware of libraries that can work, but I don't want to wrestle with pip
-    cellwidth = 11
-    tablewidth = cellwidth * 7
+    global timetabledata
+    timetabledata = timetabledata.fillna("")
     headers = timetabledata.columns.tolist()
 
-    # Print pretty little top part
+    print_spacer()
+
+    # Print header row
+    for header in headers:
+        print("│ " + header, end="")
+        if len(header) < cellwidth:
+            for i in range(cellwidth - len(header)):
+                print(" ", end="")
+    print("│")
+
+    print_spacer()
+
+    # Print data rows
+
+    print_data_rows()
+
+    print()
+    print()
+
+def print_spacer():
+    # Prints a spacer for each header, conforing to spacing
+    headers = timetabledata.columns.tolist()
     for header in headers:
         print("+",end="")
         for i in range(cellwidth+1):
             print("─", end="")
     print("+")
 
-    # Print header row
-    for header in headers:
-        print("| " + header, end="")
-        if len(header) < cellwidth:
-            for i in range(cellwidth - len(header)):
-                print(" ", end="")
-    print("|")
+def print_data_rows():
+    # Iterate through every row
+    for index, row in timetabledata.iterrows():
+        for header in timetabledata.columns:
+            data = str(row[header])
+            print("│ " + data, end="")
+            if len(data) < cellwidth:
+                for i in range(cellwidth - len(data)):
+                    print(" ", end="")
+        print("│")
+        print_spacer()
 
-    
-    print()
-    print()
-
+        print(timetabledata)
 
 def edit_mode_UI():
-    clearterm()
+    clear_terminal()
 
 def edit_data():
     pass
